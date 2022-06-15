31fca72ddae2b91b931ccffdd9648c8d2c2a2e1f
kingbarz <mrnwobodo@gmail.com>
2022-06-15 15:36:58 +0100

    Second commit: All functionalities are in place

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import smtplib
-
+import os
 from flask import Flask, render_template, request, send_from_directory
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'DraconianDevil'
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 Bootstrap(app)
+
+app_password = os.environ["APP_PASSWORD"]
 
 
 @app.route("/")
@@ -18,14 +20,17 @@ def contact():
     if request.method == "POST":
         data = request.form
         send_email(data["name"], data["email"], data["subject"], data["message"])
+
+        return render_template("index.html")
+
     return render_template("contact2.html")
 
 
 def send_email(name, email, subject, message):
     email_message = f"Subject:{subject}\n\nName: {name}\nEmail: {email}\nMessage:{message}"
-    with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
-        connection.login(user="pythondavid123@gmail.com", password="Draconian123")
-        connection.sendmail(from_addr="pythondavid123@gmail.com", to_addrs="viperdavid123@yahoo.com",
+    with smtplib.SMTP_SSL("smtp.mail.yahoo.com") as connection:
+        connection.login(user="viperdavid123@yahoo.com", password=app_password)
+        connection.sendmail(from_addr="viperdavid123@yahoo.com", to_addrs="pythondavid123@gmail.com",
                             msg=email_message)
 
 
